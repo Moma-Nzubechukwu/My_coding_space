d5f3c8b71940722fbbc93ed690c0255061dd9092
Nzube root <momafransisco@gmail.com>
2025-01-17 08:39:55 +0000

    add| ocupamt info

--- my_python_programms/praticepython/HOTELS/occupants_details.py
+++ my_python_programms/praticepython/HOTELS/occupants_details.py
@@ -1,7 +1,12 @@
 
 
 
-
+has = input("room no")
 name =input("enter occupants name")
 phoneno = input("enter occupants phone number")
-file = open(
+room = "ROOM"+has+"/ocupant_info"
+file = open(room, 'w+')
+gash = 'name :'+name+"\n"
+phno = "phone number : "+phoneno+"\n"
+task = file.writelines([gash, phno])
+file.close()
